1089cc7bcf5e82f49bc80f2fcb01ae1002fd1f16
zoro_gauken <netfliks@gmail.com>
2024-07-19 16:28:25 +0500

    калькулятор

---
+++ index.js
@@ -0,0 +1,20 @@
+let a = +prompt('a')
+let method = prompt('+ - * /')
+let b = +prompt('b')
+switch (method) {
+    case '+':
+    alert(a + b)
+        break
+        case '-':
+    alert(a + b)
+        break
+        case '*':
+    alert(a + b)
+        break
+        case '/':
+    alert(a + b)
+        break
+
+    default:
+        alert('кыш')
+}
